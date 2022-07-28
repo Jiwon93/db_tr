b1dcb7266e40a3bdc8f206c1f950e8273caad6d4
Jiwon93 <ls_sjw93@naver.com>
2022-07-28 14:47:18 +0900

    practice

--- mysql/sql/basic.sql
+++ mysql/sql/basic.sql
@@ -1,8 +1,21 @@
 -- espresso database를 사용하겠다. 
 use espresso;
 
+INSERT INTO member2 (
+	name
+    ,nameEng
+    ,birthday
+)
+
+VALUES (
+	"고수"
+    ,"Gosu"
+    ,"1992-02-10"
+)
+;
+
 -- 전체 컬럼 조회
-SELECT * from member;
+SELECT * from member2;
 
 -- 컬럼 추가
 ALTER TABLE member2 ADD COLUMN name varchar(45);
@@ -17,3 +30,19 @@ ALTER TABLE member2 MODIFY COLUMN bod date;
 
 -- 컬럼 이름 변경
 ALTER TABLE member2 CHANGE COLUMN bod birthday date;
+
+-- 컬럼 삭제
+ALTER TABLE member2 drop COLUMN nameEng;
+
+-- row 삭제
+DELETE FROM member WHERE mmSeq = 10;
+
+-- commit / rollback
+
+-- 데이터 수정
+UPDATE member2 SET
+	name = "신지원"
+	, nameEng = "Shinjiwon"
+	, birthday = "1993-02-10"
+WHERE mmseSeq = 1;
+
