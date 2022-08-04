afbbaa628c0573f38081f24391e081c82a72e300
Jiwon93 <ls_sjw93@naver.com>
2022-08-04 15:38:21 +0900

    join

--- mysql/sql/basic_select.sql
+++ mysql/sql/basic_select.sql
@@ -11,7 +11,9 @@ SELECT
     ,a.mmAddress2
     ,b.mmpPhoneNumber
 FROM member a
+-- 전부 보여주기
 -- left join member_phone b on b.member_mmseq = a.mmSeq
+-- 채워진 항목만 보여주기
 -- inner join member_phone b on b.member_mmseq = a.mmSeq
 join member_phone b on b.member_mmseq = a.mmSeq
 ;
