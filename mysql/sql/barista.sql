ea15901e1c23d9664c47bedf36a7172ea0b1baea
Jiwon93 <ls_sjw93@naver.com>
2022-08-05 12:52:03 +0900

    db

--- mysql/sql/barista.sql
+++ mysql/sql/barista.sql
@@ -17,19 +17,38 @@ FROM member
 WHERE mmEmail = "jihun@naver.com" and mmPw = "jihun123"
 ;
 
--- 아이디 찾기
-SELECT 
-	a.mmName
-    ,b.mmpPhoneNumber
+-- 이메일 찾기
+SELECT a.mmEmail
 FROM member a 
 inner join member_phone b on b.member_mmSeq = a.mmSeq
-WHERE a.mmName = "박지훈" and b.mmpPhoneNumber = "01001010101"
+WHERE a.mmName = "" and b.mmpPhoneNumber = ""
+;
+
+SELECT 
+	b.mmpPhoneNumber
+FROM member a
+inner join member_phone b on b.member_mmSeq = a.mmSeq
+WHERE a.mmEmail = ""
 ;
 
 -- 비밀번호 찾기
-SELECT a.mmEmail 
-FROM member a 
-WHERE mmEmail = "jiwon@naver.com"; 
+SELECT mmEmail FROM member WHERE mmEmail = ""; 
+
+-- 이메일 중복확인
+SELECT mmEmail FROM member WHERE mmEmail = "";
+
+-- 닉네임 중복확인
+SELECT mmNickname FROM member WHERE mmNickname = "";
+
+-- 생일자 정보확인
+SELECT 
+	mmName
+    ,mmBod
+FROM member 
+WHERE mmName = "" and mmBod = ""
+;
+
+-- 
 
 -- 목록
 
